813b5a870f1fd41ec6855a173672fe58cfd1766c
GMTurbo <gmturbo@me.com>
2014-09-20 13:15:11 -0600

    modified tests and added event emitter reference

--- tests/test.js
+++ tests/test.js
@@ -2,8 +2,8 @@ var Differ = require('../lib/fs-dif'),
   fs = require('fs');
 
 var filename1 = '/Users/gabrieltesta/Desktop/me.jpeg',
-    filename2 = '/Users/gabrieltesta/Desktop/me1.jpeg',
-    filename3 = '/Users/gabrieltesta/Desktop/temp/me.jpeg';
+  filename2 = '/Users/gabrieltesta/Desktop/old/bolivia.zip',
+  filename3 = '/Users/gabrieltesta/Desktop/temp/me.jpeg';
 
 var fsDif = new Differ();
 
@@ -14,11 +14,19 @@ fs.readFile(filename1,
       fileName: filename1,
       size: fs.statSync(filename1).size
     });
+  });
+
+fs.readFile(filename2,
+  function(err, data) {
     fsDif.update({
       data: data,
       fileName: filename2,
-      size: fs.statSync(filename1).size
+      size: fs.statSync(filename2).size
     });
+  });
+
+fs.readFile(filename1,
+  function(err, data) {
     fsDif.update({
       data: data,
       fileName: filename3,
